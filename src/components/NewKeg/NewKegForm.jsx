8a22aa3223d22e9b3616d08aa0f567fd2a7cc65a
Amy Churchwell <amy.churchwell@gmail.com>
2018-02-16 09:40:05 -0800

    Define values in new keg form and install uuid

--- src/components/NewKeg/NewKegForm.jsx
+++ src/components/NewKeg/NewKegForm.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
 
 function NewKegForm(props) {
+  let _name = null;
+  let _brand = null;
+  let _price = null;
+  let _alcoholContent = null;
+  let _image = null;
 
   function handleNewKegFormSubmission(event) {
     event.preventDefault();
